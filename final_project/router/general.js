b5040c5a89c04f0ea94c330e83b67ea0d9544c0f
matthewjlewis93 <matthewjlewis93@gmail.com>
2024-07-31 17:24:16 -0500

    added get all books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.status(200);
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
@@ -24,8 +24,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 // Get all books based on title
