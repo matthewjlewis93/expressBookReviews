e1fc0b88f177f7c19ebf2f93b5868744fdfe66d7
matthewjlewis93 <matthewjlewis93@gmail.com>
2024-08-01 18:26:33 -0500

    completed tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,9 +1,15 @@
 const express = require('express');
-let books = require("./booksdb.js");
+// let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios').default;
+axios.defaults.baseURL = 'http://localhost:5000'
 
+
+public_users.get('/books', (req,res) => {
+  res.send(require("./booksdb.js"));
+})
 
 public_users.post("/register", (req,res) => {
   const { username, password } = req.body;
@@ -20,20 +26,23 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
+  let books = (await axios.get('/books')).data;
   res.status(200);
-  res.send(JSON.stringify(books,null,4));
+  res.send(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   const isbn = req.params.isbn;
+  let books = (await axios.get('/books')).data;
   res.send(books[isbn])
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   const author = req.params.author;
+  let books = (await axios.get("/books")).data
   let bookKeys = Object.keys(books);
   let bookList = {}
   bookKeys.forEach((key) => {
@@ -42,12 +51,13 @@ public_users.get('/author/:author',function (req, res) {
     }
   }) 
 
-  res.send(JSON.stringify(bookList,null,4));
+  res.send(bookList);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   const title = req.params.title;
+  const books = (await axios.get("/books")).data;
   let bookKeys = Object.keys(books);
   let bookList = {};
   bookKeys.forEach((key) => {
@@ -55,7 +65,7 @@ public_users.get('/title/:title',function (req, res) {
       bookList[key] = books[key]
     }
   })
-  res.send(JSON.stringify(bookList,null,4));
+  res.send(bookList);
 });
 
 //  Get book review
